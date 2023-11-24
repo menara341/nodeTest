55689ad0bdd9d29294c5d1c32aca76b9cfb13735
menara341 <martin.menara@outlook.com>
2023-11-24 18:00:03 +0100

    Erster Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+
+// GET-Route für die Willkommensseite
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
